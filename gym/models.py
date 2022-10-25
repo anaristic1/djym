9a205313e9f21d421f9eb661019979fbf8dee2b1
anaristic1 <ristic.ana@gmail.com>
2022-10-26 00:44:05 +0200

    init models classes

--- gym/models.py
+++ gym/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ExerciseType(models.Model):
+    type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.type
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=80)
+    type = models.ForeignKey(ExerciseType, on_delete=models.PROTECT)
+    video = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
